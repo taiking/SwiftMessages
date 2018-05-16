a6c22fae1d24c2971a87656a2136f3ddedcd5e3b
chocovayashi <c.tsujiyan727@gmail.com>
2018-05-16 12:39:57 +0900

    update demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -12,12 +12,7 @@ import SwiftMessages
 class ViewController: UITableViewController {
 
     var items: [Item] = [
-        .titleBody(title: "MESSAGE VIEW", body: "SwiftMessages provides a standard message view along with a number of layouts, themes and presentation options.", function: ViewController.demoBasics),
         .titleBody(title: "ANY VIEW", body: "Any view, no matter how cute, can be displayed as a message.", function: ViewController.demoAnyView),
-        .titleBody(title: "CUSTOMIZE", body: "Easily customize by copying one of the SwiftMessages nib files into your project as a starting point. Then order some tacos.", function: ViewController.demoCustomNib),
-        .explore,
-        .titleBody(title: "CENTERED", body: "Show cenetered messages with a fun, physics-based dismissal gesture.", function: ViewController.demoCentered),
-        .counted,
     ]
 
     /*
@@ -54,63 +49,6 @@ class ViewController: UITableViewController {
     /*
      MARK: - Demos
      */
-
-    static func demoBasics() -> Void {
-        
-        let error = MessageView.viewFromNib(layout: .tabView)
-        error.configureTheme(.error)
-        error.configureContent(title: "Error", body: "Something is horribly wrong!")
-        error.button?.setTitle("Stop", for: .normal)
-        
-        let warning = MessageView.viewFromNib(layout: .cardView)
-        warning.configureTheme(.warning)
-        warning.configureDropShadow()
-        
-        let iconText = ["🤔", "😳", "🙄", "😶"].sm_random()!
-        warning.configureContent(title: "Warning", body: "Consider yourself warned.", iconText: iconText)
-        warning.button?.isHidden = true
-        var warningConfig = SwiftMessages.defaultConfig
-        warningConfig.presentationContext = .window(windowLevel: UIWindowLevelStatusBar)
-
-        let success = MessageView.viewFromNib(layout: .cardView)
-        success.configureTheme(.success)
-        success.configureDropShadow()
-        success.configureContent(title: "Success", body: "Something good happened!")
-        success.button?.isHidden = true
-        var successConfig = SwiftMessages.defaultConfig
-        successConfig.presentationStyle = .center
-        successConfig.presentationContext = .window(windowLevel: UIWindowLevelNormal)
-
-        let info = MessageView.viewFromNib(layout: .messageView)
-        info.configureTheme(.info)
-        info.button?.isHidden = true
-        info.configureContent(title: "Info", body: "This is a very lengthy and informative info message that wraps across multiple lines and grows in height as needed.")
-        var infoConfig = SwiftMessages.defaultConfig
-        infoConfig.presentationStyle = .bottom
-        infoConfig.duration = .seconds(seconds: 0.25)
-
-        let status = MessageView.viewFromNib(layout: .statusLine)
-        status.backgroundView.backgroundColor = UIColor.purple
-        status.bodyLabel?.textColor = UIColor.white
-        status.configureContent(body: "A tiny line of text covering the status bar.")
-        var statusConfig = SwiftMessages.defaultConfig
-        statusConfig.presentationContext = .window(windowLevel: UIWindowLevelStatusBar)
-
-        let status2 = MessageView.viewFromNib(layout: .statusLine)
-        status2.backgroundView.backgroundColor = UIColor.orange
-        status2.bodyLabel?.textColor = UIColor.white
-        status2.configureContent(body: "Switched to light status bar!")
-        var status2Config = SwiftMessages.defaultConfig
-        status2Config.presentationContext = .window(windowLevel: UIWindowLevelNormal)
-        status2Config.preferredStatusBarStyle = .lightContent
-
-        SwiftMessages.show(view: error)
-        SwiftMessages.show(config: warningConfig, view: warning)
-        SwiftMessages.show(config: successConfig, view: success)
-        SwiftMessages.show(config: infoConfig, view: info)
-        SwiftMessages.show(config: statusConfig, view: status)
-        SwiftMessages.show(config: status2Config, view: status2)
-    }
     
     static func demoAnyView() -> Void {
         let imageView = UIImageView()
@@ -119,52 +57,26 @@ class ViewController: UITableViewController {
         imageView.clipsToBounds = true
         let messageView = BaseView(frame: .zero)
         messageView.layoutMargins = .zero
-        messageView.preferredHeight = 120.0
+        messageView.preferredHeight = 220.0
         if #available(iOS 11, *) {
             // Switch to a card-style layout for iOS 11 because the image
             // doesn't fit well behind the notch. Need to install a background
             // view for the drop shadow.
             let backgroundView = UIView()
-            backgroundView.layer.cornerRadius = 10
-            imageView.layer.cornerRadius = 10
+            backgroundView.layer.cornerRadius = 100
+            imageView.layer.cornerRadius = 100
             messageView.installBackgroundView(backgroundView)
             messageView.installContentView(imageView, insets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
             messageView.safeAreaTopOffset = -6
         } else {
             messageView.installContentView(imageView)
         }
+        messageView.dismissMoveQuantity = 100
         messageView.configureDropShadow()
         var config = SwiftMessages.defaultConfig
         config.presentationContext = .window(windowLevel: UIWindowLevelStatusBar)
-        SwiftMessages.show(config: config, view: messageView)
-    }
-
-    static func demoCustomNib() {
-        let view: TacoDialogView = try! SwiftMessages.viewFromNib()
-        view.configureDropShadow()
-        view.getTacosAction = { _ in SwiftMessages.hide() }
-        view.cancelAction = { SwiftMessages.hide() }
-        var config = SwiftMessages.defaultConfig
-        config.presentationContext = .window(windowLevel: UIWindowLevelStatusBar)
-        config.duration = .forever
-        config.presentationStyle = .bottom
-        config.dimMode = .gray(interactive: true)
-        SwiftMessages.show(config: config, view: view)
-    }
-
-    static func demoCentered() {
-        let messageView: MessageView = MessageView.viewFromNib(layout: .centeredView)
-        messageView.configureBackgroundView(width: 250)
-        messageView.configureContent(title: "Hey There!", body: "Please try swiping to dismiss this message.", iconImage: nil, iconText: "🦄", buttonImage: nil, buttonTitle: "No Thanks") { _ in
-            SwiftMessages.hide()
-        }
-        messageView.backgroundView.backgroundColor = UIColor.init(white: 0.97, alpha: 1)
-        messageView.backgroundView.layer.cornerRadius = 10
-        var config = SwiftMessages.defaultConfig
         config.presentationStyle = .center
-        config.duration = .forever
-        config.dimMode = .blur(style: .dark, alpha: 1, interactive: true)
-        config.presentationContext  = .window(windowLevel: UIWindowLevelStatusBar)
+        config.isAutoHide = false
         SwiftMessages.show(config: config, view: messageView)
     }
 }
